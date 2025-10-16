e350242e762488e20acf9c0e0ded4f68a1f1a269
PaulGuerra07 <gpaul4342@gmail.com>
2025-10-16 17:10:55 -0500

    Add files via upload

---
+++ DEBER 1 EJERCICIO 2.cpp
@@ -0,0 +1,47 @@
+/*Ejercicio 2
+Escriba un programa que calcule el impuesto de un producto basado en 
+su precio y categoría, utilizando operadores ternarios. 
+1. El programa debe solicitar al usuario ingresar el precio del producto y 
+su categoría, donde las categorías son: 
+"Electrónica" (se aplica un 20% de impuesto). 
+"Ropa" (se aplica un 10% de impuesto). 
+"Alimentos" (se aplica un 5% de impuesto). 
+2. Si el precio es mayor a 1000, se aplica un descuento del 10% sobre el 
+precio original antes de calcular el impuesto. 
+3. Usa operadores ternarios para: 
+Determinar el impuesto según la categoría. 
+Aplicar el descuento si el precio es mayor a 1000. 
+Calcular el precio final después de aplicar impuestos y 
+descuentos.*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{ 
+    float impuesto, descuento=0.0, precio_producto, precio_final, precio_descuento;
+    string categoria;
+
+    cout <<"Ingrese precio del producto : "<<endl;
+    cin >> precio_producto;
+    cout <<"Ingrese categoria del producto (Electronica, Ropa, Alimentos) : "<<endl;
+    cin >> categoria;
+
+    descuento=(precio_producto>1000)? precio_producto*0.10:
+    0.0;
+    precio_descuento=precio_producto-descuento;
+
+    impuesto=(categoria=="Electronica")? precio_descuento*0.20:
+    (categoria=="Ropa")? precio_producto*0.10:
+    (categoria=="Alimentos")? precio_producto*0.05:
+    0.0;
+
+    precio_final=precio_descuento+impuesto;
+    cout <<"Recibo de venta: "<<endl;
+    cout <<"Precio producto: "<<precio_producto<<endl;
+    cout <<"Descuento: "<<descuento<<endl;
+    cout <<"Impuesto: "<<impuesto<<endl;
+    cout <<"Precio final: "<<precio_final<<endl;
+
+    return 0;
+}
---
+++ DEBER 1 EJERCICIO 3.cpp
@@ -0,0 +1,55 @@
+/*Ejercicio 3
+Escriba un programa en C++: 
+• Solicite al usuario una temperatura en grados Celsius (permitir 
+decimales). 
+• Verifica si la temperatura es válida (debe estar entre -100 y 100). Si no 
+lo es, muestra un mensaje de error. 
+• Convierte la temperatura a grados Fahrenheit con la fórmula: F = C * 
+9 / 5 + 32; 
+• Clasifica la temperatura en Celsius usando operador ternario 
+anidado: 
+a. Menor a 0 °C: “Muy fría” 
+b. Entre 0 °C y 15 °C: “Fría” 
+c. Entre 15 °C y 30 °C: “Templada” 
+d. Mayor a 30 °C: “Caliente” 
+• Además, solicita una temperatura umbral de comparación, y usa 
+expresiones lógicas y relacionales para determinar: 
+a. ¿Es mayor que el umbral? 
+b. ¿Es múltiplo de 5? o “No es múltiplo de 5”.*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+   string temperatura;
+   float grados_Celsius, grados_Fahrenheit, tempertura_umbral_de_comparacion;
+   bool mayor, multiplo;
+
+    cout <<"Ingrese una temperatura en grados Celsius entre (-100 y 100): "<<endl;
+    cin >>grados_Celsius;
+    
+    if(grados_Celsius<-100||grados_Celsius>100)
+    {
+        cout<<"Error, no se encuentra dentro del nivel permitido" <<endl;
+        return 0;
+    }
+    grados_Fahrenheit=grados_Celsius * 9 / 5 + 32;
+    temperatura=(grados_Celsius<0)? "Muy fria":
+    (grados_Celsius>=0||grados_Celsius<=15)? "Fria":
+    (grados_Celsius>15||grados_Celsius<=30)? "Templada":
+    "Caliente";
+
+    cout <<"Ingrese una temperatura umbral de comparacion" <<endl;
+    cin >>tempertura_umbral_de_comparacion;
+    mayor=grados_Celsius>tempertura_umbral_de_comparacion;
+    multiplo=static_cast<int>(grados_Celsius)%5==0;
+
+    cout << "\n--- RESULTADOS ---" << endl;
+    cout << "Temperatura en Celsius: " << grados_Celsius << "°C" << endl;
+    cout << "Temperatura en Fahrenheit: " << grados_Fahrenheit << "°F" << endl;
+    cout << "Clasificación: " << temperatura << endl;
+    cout << "¿Es mayor que " << tempertura_umbral_de_comparacion << "? " 
+         << (mayor ? "Si" : "No") << endl;
+    cout << "¿Es múltiplo de 5? " << (multiplo ? "Si" : "No") << endl;
+}
